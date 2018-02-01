e0a2c1c4112f20c8e4d04eb73a9600123e250378
kayejor <kayejor@umich.edu>
2018-01-31 22:35:24 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type Player struct {
+	ws   *websocket.Conn
+	name string
+	team int
+}
+
+var players []Player
+var playersMux sync.Mutex
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+	http.HandleFunc("/websocket", handleConnections)
+	http.Handle("/", fs)
+	rand.Seed(time.Now().UnixNano())
+	http.ListenAndServe(":3000", nil)
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type PlayerRole struct {
+	Name string `json:"name"`
+	Role int    `json:"role"`
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	var msg PlayerRole
+	err = conn.ReadJSON(&msg)
+	if err != nil {
+		log.Fatal("problem with message")
+		log.Fatal(msg)
+		return
+	}
+	player := Player{
+		ws:   conn,
+		name: string(msg.Name),
+		team: msg.Role,
+	}
+	players = append(players, player)
+	broadcastNames()
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			removeConnection(conn)
+			broadcastNames()
+		}
+		if string(msg) == "start" {
+			presentInformationToAllPlayers()
+		}
+	}
+}
+
+func broadcastNames() {
+	playersMux.Lock()
+	var namesHTML = createHTMLListFromNames()
+	for _, player := range players {
+		player.ws.WriteMessage(websocket.TextMessage, []byte(namesHTML))
+	}
+	playersMux.Unlock()
+}
+
+func removeConnection(conn *websocket.Conn) {
+	playersMux.Lock()
+	for index := 0; index < len(players); index++ {
+		if conn == players[index].ws {
+			players = append(players[:index], players[index+1:]...)
+			break
+		}
+	}
+	playersMux.Unlock()
+}
+
+func createHTMLListFromNames() string {
+	var result = ""
+	for _, player := range players {
+		result += "<li>" + player.name + "</li>"
+	}
+	return result
+}
+
+func createAllKnowingList() string {
+	var result = ""
+	for _, player := range players {
+		result += "<li class=" + getClass(player.team) + ">" + player.name + "</li>"
+	}
+	return result
+}
+
+func getClass(i int) string {
+	if i == 0 {
+		return "liberal"
+	} else if i == 1 {
+		return "fascist"
+	} else {
+		return "hitler"
+	}
+}
+
+func presentInformationToAllPlayers() {
+	playersMux.Lock()
+	var namesHTML = createHTMLListFromNames()
+	var allKnowingNamesHTML = createAllKnowingList()
+	for _, player := range players {
+		if player.team == 1 || player.team == 2 && len(players) < 7 {
+			player.ws.WriteMessage(websocket.TextMessage, []byte(allKnowingNamesHTML))
+		} else {
+			player.ws.WriteMessage(websocket.TextMessage, []byte(namesHTML))
+		}
+	}
+	playersMux.Unlock()
+}
---
+++ public/app.js
@@ -0,0 +1,33 @@
+function myWebsocketStart(role)
+{
+    var ws = new WebSocket("ws://10.0.0.146:3000/websocket");
+
+    ws.onopen = function() {
+        var name = document.getElementById("name").value;
+        var json = '{"name":"' + name + '", "role":' + role + '}';
+        ws.send(json);
+        document.getElementById("name").remove();
+        document.getElementById("libBtn").remove();
+        document.getElementById("fascBtn").remove();
+        document.getElementById("hitBtn").remove();
+        
+        var button = document.createElement("button");
+        button.innerHTML = "Start!";
+        button.className = "button";
+        button.addEventListener ("click", function() {
+        ws.send("start");
+        });
+        document.getElementsByTagName("body")[0].appendChild(button);
+    }
+
+    ws.onmessage = function (evt)
+    {
+        var personList = document.getElementById("personList");
+        personList.innerHTML = (evt.data);
+    };
+
+    ws.onclose = function()
+    {
+        console.log("I am now closed");
+    };
+}
